a1ec679b35ea7479d24fec03fd68193830cb080b
Chester Omotosho-Gaywood <78214004+CGaywood@users.noreply.github.com>
2026-02-18 04:52:29 +0000

    More small additions
    Added some really bad placeholder animations for NPC and player
    Added very basic inventory

---
+++ elf-game/area_trigger.gd
@@ -0,0 +1,9 @@
+extends Area2D
+## Gives the player the option to move to another zone when entered
+
+# This var should be exported, but I'm not yet sure what the type should be
+# Or what exactly it should refer to
+var destination
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
--- elf-game/player.gd
+++ elf-game/player.gd
@@ -1,23 +1,30 @@
 extends CharacterBody2D
 
 
-@export var speed:float = 200
+
 var direction
+
+@export var speed:float = 200
+
+# ---Player Inventory---
+# This dictionary holds all of the items the player can possibly possess
+# True if the player owns the item
+# False if they don't
+var inventory:Dictionary[String, bool] = {
+	"placeholder_item": false,
+	"placeholder_item2": false
+}
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _physics_process(delta: float) -> void:
 	
 	# Player Movement
-	direction = Input.get_vector("left", "right", "up", "down")
+	direction = Input.get_vector("left", "right", "up", "down").normalized()
 	velocity = direction*speed
 	
 	move_and_slide()
