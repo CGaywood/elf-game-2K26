68a27726d10be4f293c767affe2ca87df2dca1ba
Chester Omotosho-Gaywood <78214004+CGaywood@users.noreply.github.com>
2026-02-19 15:35:25 +0000

    Some more things
    Added a GameManager to handle the game's logic across scenes

--- elf-game/area_trigger.gd
+++ elf-game/area_trigger.gd
@@ -2,8 +2,11 @@ extends Area2D
 ## Gives the player the option to move to another zone when entered
 
 # This var should be exported, but I'm not yet sure what the type should be
-# Or what exactly it should refer to
+# Or what exactly it should refer to (zone, or specific location in zone)
 var destination
 
+# This might be unnecessary but whatever
+signal zone_change(destination)
+
 func _on_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+	zone_change.emit(destination)
--- elf-game/player.gd
+++ elf-game/player.gd
@@ -6,15 +6,6 @@ var direction
 
 @export var speed:float = 200
 
-# ---Player Inventory---
-# This dictionary holds all of the items the player can possibly possess
-# True if the player owns the item
-# False if they don't
-var inventory:Dictionary[String, bool] = {
-	"placeholder_item": false,
-	"placeholder_item2": false
-}
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
