3fa4a1fcb3acf3438a70975368804d4b1f35f52b
LykAunn <lykaunn@gmail.com>
2026-02-21 19:56:41 +0000

    Music fix

---
+++ elf-game/world.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var level_music: AudioStream
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if level_music:
+		print("plating music")
+		MusicManager.play_music(level_music, 2.0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
